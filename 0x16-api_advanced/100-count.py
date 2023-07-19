4bdab404f07207d9e86ef1dfc4a7d2a2a7619e95
root <biyahon@gmail.com>
2023-07-19 14:02:21 +0000

    task last

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,46 +1,72 @@
 #!/usr/bin/python3
-""" Read data from the reddit API. """
-import json
+"""
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
+"""
+import re
 import requests
+import sys
+
+
+def add_title(dictionary, hot_posts):
+    """ Adds item into a list """
+    if len(hot_posts) == 0:
+        return
+
+    title = hot_posts[0]['data']['title'].split()
+    for word in title:
+        for key in dictionary.keys():
+            c = re.compile("^{}$".format(key), re.I)
+            if c.findall(word):
+                dictionary[key] += 1
+    hot_posts.pop(0)
+    add_title(dictionary, hot_posts)
+
+
+def recurse(subreddit, dictionary, after=None):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'after': after
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+
+    if res.status_code != 200:
+        return None
+
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    add_title(dictionary, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return
+    recurse(subreddit, dictionary, after=after)
 
 
 def count_words(subreddit, word_list):
-    """ Queries the Reddit API and returns the count of given words
-    based on a givenn subreddit and word list:
-        Args:
-            subreddit (str):
-            word_list (list): word list
-    """
-    # Build the query string
-    if after == "":
-        AFTER = ""
+    """ Init function """
+    dictionary = {}
+
+    for word in word_list:
+        dictionary[word] = 0
+
+    recurse(subreddit, dictionary)
+
+    l = sorted(dictionary.items(), key=lambda kv: kv[1])
+    l.reverse()
+
+    if len(l) != 0:
+        for item in l:
+            if item[1] is not 0:
+                print("{}: {}".format(item[0], item[1]))
     else:
-        AFTER = "?after={}".format(after)
-
-    # build page list
-    headers = {"User-Agent": "alx2-web-app"}
-    url = "https://www.reddit.com/r/{}/hot.json{}".format(subreddit, AFTER)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    page_list = []
-    if res.status_code != 200:
-        return None
-    if "children" in res.json()["data"]:
-        for child in res.json()["data"]["children"]:
-            page_list.append(child["data"]["title"])
-
-    hot_list += page_list
-    if "after" not in res.json()["data"]:
-        return hot_list
-    after = res.json()["data"]["after"]
-    if after is None:
-        return hot_list
-
-    return recurse(subreddit, hot_list, after)
-
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 3:
-        print("Search argument required")
-        exit()
-    print(len(count_words(sys.argv[1]), sys.argv[2]))
+        print("")
